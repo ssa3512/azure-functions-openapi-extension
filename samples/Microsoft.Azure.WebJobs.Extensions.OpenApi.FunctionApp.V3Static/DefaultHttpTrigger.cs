d889f35c9c16fa9f7eb5b7021f96a59647254962
Steve Ashman <steve@ash.mn>
2021-04-02 19:28:44 -0400

    Repro issue 64

--- samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.V3Static/DefaultHttpTrigger.cs
+++ samples/Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.V3Static/DefaultHttpTrigger.cs
@@ -39,5 +39,65 @@ namespace Microsoft.Azure.WebJobs.Extensions.OpenApi.FunctionApp.V3Static
 
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("GetModelOne")]
+        [OpenApiOperation(operationId: "GetModelOne", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(GenericModel<TestModelOne>), Description = "The OK response")]
+        public static IActionResult GetModelOne(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var response = new GenericModel<TestModelOne>()
+            {
+                Data = new TestModelOne()
+                {
+                    Name = "Hello World",
+                    Age = 25
+                },
+                Message = "Hello"
+            };
+            return new OkObjectResult(response);
+        }
+
+        [FunctionName("GetModelTwo")]
+        [OpenApiOperation(operationId: "GetModelTwo", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(GenericModel<TestModelTwo>), Description = "The OK response")]
+        public static IActionResult GetModelTwo(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var response = new GenericModel<TestModelTwo>()
+            {
+                Data = new TestModelTwo()
+                {
+                    Class = "Coding",
+                    Instructor = "Professor Azure"
+                },
+                Message = "Hello"
+            };
+            return new OkObjectResult(response);
+        }
+
+        public class GenericModel<T>
+        {
+            public string Message { get; set; }
+            public T Data { get; set; }
+        }
+
+        public class TestModelOne
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class TestModelTwo
+        {
+            public string Class { get; set; }
+            public string Instructor { get; set; }
+        }
     }
 }
